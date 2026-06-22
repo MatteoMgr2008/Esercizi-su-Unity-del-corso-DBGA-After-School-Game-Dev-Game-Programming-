09e0e2799fa88b69481761a9e914b0935bad9a67
Matteo Magrino DBGA <matteo.mgr8@outlook.it>
2026-06-22 13:56:26 +0200

    Added 2nd Unity project exercise

---
+++ Es. 2 (il Coin Collider)/Assets/Scripts/CoinCollider.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class CoinCollider : MonoBehaviour
+{
+    // Start è chiamato una volta prima della prima esecuzione di Update dopo che il MonoBehaviour è stato creato
+    void Start()
+    {
+        
+    }
+
+    // Update viene chiamato una volta per frame
+    void Update()
+    {
+        
+    }
+}
